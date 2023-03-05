08340d9a74c9f3008966583e9ce7280a585e914c
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-03-04 19:08:55 -0500

    Django tutorial, part 1

---
+++ pugberus/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pugberus project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pugberus.settings')
+
+application = get_wsgi_application()
