3ea2e55efea5f1c0cd85f82926b1d9b7ddb227e8
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-04-15 07:35:15 -0400

    Scoring module, part 3 - init views

--- league/urls.py
+++ league/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'league'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('p/<int:player_id>', views.player, name='player'),
+    path('l/<int:league_id>', views.league, name='league'),
+    path('g/<int:game_id>', views.game, name='game'),
 ]
--- league/views.py
+++ league/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from league.models import Game, League, Player
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the league index.")
+    leagues = League.objects.all()
+    context = {
+        'leagues': leagues,
+    }
+    return render(request, 'league/index.html', context)
+
+def league(request, league_id):
+    _league = get_object_or_404(League, pk=league_id)
+    return render(request, 'league/league.html', {'league': _league})
+
+def player(request, player_id):
+    _player = get_object_or_404(Player, pk=player_id)
+    return render(request, 'league/player.html', {'player': _player})
+
+def game(request, game_id):
+    _game = get_object_or_404(Game, pk=game_id)
+    return render(request, 'league/game.html', {'game': _game})
