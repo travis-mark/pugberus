7c837bc87f4c118536e377ad09aa3e91fb850ae4
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-04-25 22:30:03 -0400

    Update polls CSS

--- polls/models.py
+++ polls/models.py
@@ -6,10 +6,8 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
     def __str__(self):
         return self.question_text
-    
     @admin.display(
         boolean=True,
         ordering='pub_date',
@@ -18,14 +16,14 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    def votes_total(self):
+        return sum(choice.votes for choice in self.choice_set.all())
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
     def __str__(self):
         return self.choice_text
-
     def votes_percent(self):
-        return 100 * self.votes / sum(choice.votes for choice in self.question.choice_set.all())
+        return 100 * self.votes / self.question.votes_total()
